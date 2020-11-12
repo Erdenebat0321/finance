7784c064ef779948388033f1459b80950f3df704
Erdenebat G <erdenebatmcu@gmail.com>
2020-11-12 13:22:21 +0800

    Finance moduliig hiij ehelsen. Inc , Exp obj uusgene

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
-var uiController = (() => {})();
+var uiController = (() => {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescrition: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
 
-var financeController = (() => {})();
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescrition).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDomStrings: () => {
+      return DOMstrings;
+    },
+  };
+})();
+
+var financeController = (() => {
+  var Income = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+    
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+})();
 
 var appController = ((uiController, financeController) => {
   var ctrlAddItem = () => {
-    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+    console.log(uiController.getInput());
   };
-  document.querySelector(".add__btn").addEventListener("click", () => {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", (event) => {
-    if (event.key === "Enter" || event.which === 13) {
+
+  var setupEventListeners = () => {
+    var DOM = uiController.getDomStrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", () => {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", (event) => {
+      if (event.key === "Enter" || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: () => {
+      console.log("App started");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
